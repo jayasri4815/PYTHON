5dabcda86fb2ffae70aa404a75bf63df2d92d271
JAYASRI SINGAMSETTI <jayasrisingamsetti2005@gmail.com>
2025-06-30 10:05:34 +0530

    Add files via upload

---
+++ days program.py
@@ -0,0 +1,8 @@
+num=int(input("enter the days.."))
+if num<=5
+            print("the charge is...,num*2")
+       elif num>=11 and num<=15:
+           print("thecharge is...",num*3)
+       elif num>=11 and num<=15:
+             else
+               print("wrong days selected")     
