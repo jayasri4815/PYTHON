216258f23081d6bdfd63564a27ed3abc4605dfa1
JAYASRI SINGAMSETTI <jayasrisingamsetti2005@gmail.com>
2025-07-15 15:51:03 +0530

    Add files via upload

---
+++ express delivery.py
@@ -0,0 +1,7 @@
+weight=int(input["enter the weight--"])
+if weight:4
+print("the delivery can be expected with in 24hrs")
+if weight<=10:
+    print("need to pay any extra amount for extra weight--")
+else:
+    print("no need to pay any extra charge have a  great delivery--")
