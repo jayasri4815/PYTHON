9af1070292a16ae8b99498edc98b9b8593dd78ab
JAYASRI SINGAMSETTI <jayasrisingamsetti2005@gmail.com>
2025-07-15 16:10:41 +0530

    Add files via upload

---
+++ nested function.py
@@ -0,0 +1,8 @@
+# a ftn which is called nested ftns
+def outerfunction():
+    print("outer function statements")
+def innerfunction():
+    print ("inner function statements")   
+innerfunction()
+outerfunction()
+ 
