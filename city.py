b7ebe15cfbbff2a31adb2ba3c5d27c888f68aa80
JAYASRI SINGAMSETTI <jayasrisingamsetti2005@gmail.com>
2025-07-15 15:51:50 +0530

    Add files via upload

---
+++ city.py
@@ -0,0 +1,13 @@
+city=input("enter a city")
+if city=="Agra":  
+   print=("Tajmahal")
+elif city=="hyderabad" :
+   print("chraminar")
+elif city=="delhi" :
+    print=("redfort")
+elif city=="mumbai":
+    print("gate way of india")
+elif city=="chennai":
+    print("merina beach")
+else:
+    print("wrong city selected")
