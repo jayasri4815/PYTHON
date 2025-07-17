ed347aaf13209ab09d048f59c6da79ce06a7e585
JAYASRI SINGAMSETTI <jayasrisingamsetti2005@gmail.com>
2025-07-17 09:54:24 +0530

    Add files via upload

---
+++ key arguments.py
@@ -0,0 +1,5 @@
+#keyword args is a ftn which take single parameter for mutliple arg in the form of key value pairs 
+#so that the output is in dictionary  format
+def function2(jayasri):
+    print("hi",jayasri)
+function2("name:jayasri,place:sayampalem")    
